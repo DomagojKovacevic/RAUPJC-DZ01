7379cb1983b520e022541f1124f9ebdb3888d1a0
Domagoj Kovacevic <domagoj.kovacevic3@fer.hr>
2016-10-20 22:13:25 +0200

    zadatak01

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialSize;
+            Console.WriteLine("Insert array size: ");
+            initialSize = Convert.ToInt32(Console.ReadLine());
+            IntegerList ListDefault = new IntegerList();
+            IntegerList ListUser = new IntegerList(initialSize);
+
+
+            ListUser.ListExample(ListUser);
+
+
+        }
+    }
+}
+
+
